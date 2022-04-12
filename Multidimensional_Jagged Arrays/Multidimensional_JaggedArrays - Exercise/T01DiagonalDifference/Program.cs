7b0ef1bfa37a9caba1366851439173463bd73fa3
IvaSabotinova <96121572+IvaSabotinova@users.noreply.github.com>
2022-04-12 17:49:55 +0300

    Update Program.cs

--- Multidimensional_Jagged Arrays/Multidimensional_JaggedArrays - Exercise/T01DiagonalDifference/Program.cs
+++ Multidimensional_Jagged Arrays/Multidimensional_JaggedArrays - Exercise/T01DiagonalDifference/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+using System;
+using System.Linq;
 
 namespace T01DiagonalDifference
 {
@@ -6,20 +7,41 @@ namespace T01DiagonalDifference
     {
         static void Main(string[] args)
         {
+            int size = int.Parse(Console.ReadLine());
+
+            int[,] squareMatrix = new int[size, size];
+
+            int sumPrimaryDiagonal = 0;
+            int sumSecondaryDiagonal = 0;
+            for (int i = 0; i < size; i++)
+            {
+                int[] currentRow = ReadArrayFromConsole();
+                for (int j = 0; j < size; j++)
+                {
+
+                    squareMatrix[i, j] = currentRow[j];
+                    if (i == j)
+                    {
+                        sumPrimaryDiagonal += squareMatrix[i, j];
+                    }
+
+                }
+            }
+
             
+            for (int i = 0; i < size; i++)
+            {
+                sumSecondaryDiagonal += squareMatrix[i, size-1 - i];
+            }
 
+            Console.WriteLine(Math.Abs(sumPrimaryDiagonal-sumSecondaryDiagonal));
+            
+        }
 
-
-
-
-
-
-
-
-
-
-
-
+        private static int[] ReadArrayFromConsole()
+        {
+            return Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse)
+                .ToArray();
         }
     }
 }
